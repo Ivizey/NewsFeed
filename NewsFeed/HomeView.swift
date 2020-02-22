f185aa4f6a17c5fef0e6b882a0cad53908129443
Pavel Bondar <ivizey@gmail.com>
2020-02-22 16:29:02 +0200

    rename ContentView

--- NewsFeed/HomeView.swift
+++ NewsFeed/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  NewsFeed
 //
 //  Created by Pavel Bondar on 22.02.2020.
@@ -8,14 +8,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
         Text("Hello, World!")
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        HomeView()
     }
 }
